79a5955770a62af1acb026befa6fef9a3e812631
Syed Andleeb <andleebsyed75@gmail.com>
2021-08-29 14:40:51 +0530

    update: added postconfig file as the tailwind was not working, updated postcss package and added a color to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,23 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
         react: {
-          DEFAULT: '#61dafb'
-        }
+          DEFAULT: "#61dafb",
+        },
+        "dark-1": "#202124",
       },
       animation: {
-        'spin-slow': 'spin 3s linear infinite',
-      }
+        "spin-slow": "spin 3s linear infinite",
+      },
     },
   },
   variants: {
     extend: {
-      animation: ['motion-safe']
+      animation: ["motion-safe"],
     },
   },
   plugins: [],
-}
+};
