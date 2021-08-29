e22addc36e70f26d7ad9f1ec9a8cabbb0cf7246e
Syed Andleeb <andleebsyed75@gmail.com>
2021-08-29 10:36:34 +0530

    update: initial setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const App = () => {
+  return (
+    <div className="text-center flex items-center min-h-screen justify-center container mx-auto text-white">
+      <h1>Welcome to chalk</h1>
+    </div>
+  );
+};
+
+export default App;
