872b207dc0588ef854e9292d3d077ab6ffb7560a
Syed Andleeb <andleebsyed75@gmail.com>
2021-08-30 21:33:49 +0530

    update: enabled tailwind jit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  purge: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  mode: "jit",
+  purge: ["./index.html", "./src/**/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
