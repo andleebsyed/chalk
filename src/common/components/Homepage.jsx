f46b154f2506e392b2f8d2d661062c75604b4fb9
Syed Andleeb <andleebsyed75@gmail.com>
2021-08-31 13:48:59 +0530

    update: took the Main content of Homepage to a separate component

--- src/common/components/Homepage.jsx
+++ src/common/components/Homepage.jsx
@@ -3,15 +3,21 @@ import { Route, Routes } from "react-router";
 import { Notes } from "../../features/notes/AllNotes";
 import { Header } from "./Header";
 export function Homepage() {
+    function HomeContent() {
+        return (
+          <div className="text-center flex flex-col items-center min-h-screen justify-center container mx-auto">
+            <h1>Welcome to chalk</h1>
+          </div>
+        );
+    }
   return (
     <div>
       <Header />
-      <Routes>
+          <Routes>
+              <Route path="/" element={<HomeContent />} />
         <Route path="/notes" element={<Notes />} />
       </Routes>
-      <div className="text-center flex flex-col items-center min-h-screen justify-center container mx-auto">
-        <h1>Welcome to chalk</h1>
-      </div>
+    
     </div>
   );
 }
