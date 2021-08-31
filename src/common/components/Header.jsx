733ab6e95f4a7fe716efde38301ef972f2e1c311
Syed Andleeb <andleebsyed75@gmail.com>
2021-08-31 12:21:56 +0530

    update: linked the app name in header to homepage

--- src/common/components/Header.jsx
+++ src/common/components/Header.jsx
@@ -4,15 +4,18 @@ import { FiMenu, FiSearch } from "react-icons/fi";
 import { IoMoonOutline } from "react-icons/io5";
 import { useTheme } from "../hooks/useTheme";
 import { Navbar } from "./Navbar/Navbar";
+import { Link } from "react-router-dom";
 export function Header() {
   const [colorTheme, setTheme] = useTheme();
   return (
     <div className="p-2  border-b border-opacity-10 min-w-screen flex justify-between ">
       <div className="flex justify-between">
         <Navbar />
-        <p className="ml-1 self-center text-blue-700 font-bold text-xl">
-          Chalk
-        </p>
+        <Link to="/" className="self-center">
+          <p className="ml-1 self-center text-blue-700 font-bold text-xl">
+            Chalk
+          </p>
+        </Link>
       </div>
       <div className="self-center  rounded-lg flex p-1  bg-white border dark:border-opacity-0 border-black">
         <button className=" hidden sm:inline mr-2 dark:text-black ">
