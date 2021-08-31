a8a986902ea3cfb3a436d13f41c9cf3c268c8f44
Syed Andleeb <andleebsyed75@gmail.com>
2021-08-31 15:37:32 +0530

    style: made image align to left and initial text align to right of the page for better ui

--- src/common/components/Landing.jsx
+++ src/common/components/Landing.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import landing from "../../assets/landing.svg";
 export function Landing() {
   return (
-    <div className="flex flex-col min-h-screen ml-2 ">
-      <div className="m-2">
-        <h1 className=" font-extrabold text-4xl  text-yellow-main">Chalk</h1>
-        <h1 className="font-medium text-2xl">All notes under one roof</h1>
+    <div className="flex flex-col min-h-screen  justify-center ">
+      <div className="flex flex-col-reverse sm:flex-row">
+        <img src={landing} className="mr-4 sm:w-[50%] sm:h-[50%] self-start" />
+        <div className="m-2">
+          <h1 className=" font-extrabold text-4xl  text-yellow-main self-end">
+            Chalk
+          </h1>
+          <h1 className="font-medium text-2xl">All notes at one place</h1>
+        </div>
       </div>
 
-      <img src={landing} className="mr-2 sm:w-[50%] sm:h-[50%] self-center" />
       <div className="text-center m-2">
         <p className="text-3xl">Join Now!</p>
         <div className="flex flex-col flex-wrap  items-center">
