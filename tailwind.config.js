be028f286cde3d7cb1ac434d57c737151b468b96
Syed Andleeb <andleebsyed75@gmail.com>
2021-08-31 11:55:18 +0530

    style: added some custom colors for the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,10 @@ module.exports = {
           DEFAULT: "#61dafb",
         },
         "dark-1": "#202124",
+        "navitem-hover": "#e8eaed",
+        "selected-navitem-light": "#feefc3",
+        "selected-navitem-dark": "#e8eaed",
+        "navitem-hover-light": "#f1f3f4",
       },
       animation: {
         "spin-slow": "spin 3s linear infinite",
