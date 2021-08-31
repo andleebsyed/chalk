edc7d237eca7c494ebd24f481f39050e5ac3ff13
Syed Andleeb <andleebsyed75@gmail.com>
2021-08-31 15:28:59 +0530

    refactor: reordered some code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { ThemeToggle } from "./common/components/Header";
 import { Homepage } from "./common/components/Homepage";
+import { Landing } from "./common/components/Landing";
 const App = () => {
+  const authorized = true;
   return (
     <main className="text-black dark:text-white">
+      {authorized && (
+        <div className="p-2 fixed  flex   min-w-screen right-0 h-9">
+          <ThemeToggle />
+        </div>
+      )}
+
       <Routes>
-        <Route path="/home/*" element={<Homepage />} />
         <Route path="/" element={<Landing />} />
+        <Route path="/home/*" element={<Homepage />} />
       </Routes>
     </main>
   );
