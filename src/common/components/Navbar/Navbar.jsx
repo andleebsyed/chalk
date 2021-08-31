ee36fed89b430d6c09f16f12f510049bd7012458
Syed Andleeb <andleebsyed75@gmail.com>
2021-08-31 12:24:51 +0530

    update: made navbar items work as toggle too for navbar, when a user clicks something in navbar, it will take her to desired page and close the navbar

--- src/common/components/Navbar/Navbar.jsx
+++ src/common/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "../Navbar/Navbar.css";
 import { FiMenu } from "react-icons/fi";
 import { ImCancelCircle } from "react-icons/im";
+import { Link } from "react-router-dom";
 export function Navbar() {
   const [navbar, setNavbar] = useState(false);
 
@@ -17,14 +18,20 @@ export function Navbar() {
             : "nav-menu bg-white dark:bg-dark-1 border"
         }
       >
-        <ul className="w-full flex flex-col items-center">
+        <ul
+          className="w-full flex flex-col items-center"
+          onClick={() => setNavbar(!navbar)}
+        >
           <li className="rounded-full  hover:bg-navitem-hover dark:hover:bg-opacity-5 self-end  m-2">
             {" "}
             <button onClick={() => setNavbar(!navbar)} className="m-2 p-2 ">
               <ImCancelCircle size={18} />
             </button>
           </li>
-          <li className="nav-item nav-item-theme">All Notes</li>
+          <Link to="/notes">
+            <li className="nav-item nav-item-theme">All Notes</li>
+          </Link>
+
           <li className="nav-item nav-item-theme ">Label 1</li>
           <li className="nav-item nav-item-theme">Label 2</li>
           <li className="nav-item nav-item-theme">Label 3</li>
