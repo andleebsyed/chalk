f1ca67f71c1795a7771dbd2d5191fbca67f34803
Syed Andleeb <andleebsyed75@gmail.com>
2021-08-31 12:21:12 +0530

    update: made a route for homepage

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import React from "react";
 
 import { Header } from "./common/components/Header";
 import { useTheme } from "./common/hooks/useTheme";
+import { Routes, Route } from "react-router-dom";
+import { Homepage } from "./common/components/Homepage";
 const App = () => {
   return (
     <main className="text-black dark:text-white">
-      <Header />
-      <div className="text-center flex flex-col items-center min-h-screen justify-center container mx-auto">
-        <h1>Welcome to cfhalk</h1>
-      </div>
+      <Routes>
+        <Route path="//*" element={<Homepage />} />
+        {/* <Route path="/home" element={<Homepage />} /> */}
+      </Routes>
     </main>
   );
 };
