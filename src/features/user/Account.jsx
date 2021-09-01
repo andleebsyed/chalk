59f58e30fa20728da52573ebc7a8c3e00e77b0dd
Syed Andleeb <andleebsyed75@gmail.com>
2021-09-01 10:49:03 +0530

    update: wired account page with the particular thunk , so that it can get populated with desired data

--- src/features/user/Account.jsx
+++ src/features/user/Account.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAccount } from "./userSlice";
 export function Account() {
+    const { account, accountStatus } = useSelector(state => state.user)
+    const { authSetupStatus } = useSelector(state => state.auth)
+    console.log({ authSetupStatus })
+    const dispatch = useDispatch()
+    useEffect(() => {
+        if (accountStatus === "idle" && authSetupStatus) {
+            console.log("am i running")
+            dispatch(fetchAccount())
+        }
+    }, [accountStatus, authSetupStatus])
     return (
-        <div className="flex justify-around items-center">
-            <h1>hello there</h1>
-        </div>
+        account === null ?
+            <div>loading</div> :
+            <div className="flex justify-around items-center">
+                <h1>{account.name}</h1>
+            </div>
     )
 }
