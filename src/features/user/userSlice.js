c9797a2306aebedbd86ab2c59886b515c3dd4dc4
Syed Andleeb <andleebsyed75@gmail.com>
2021-09-01 10:45:45 +0530

    update: added a thunk to fetch user account from backend

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,10 +1,39 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { FETCH_ACCOUNT } from "../../services/api";
+
+export const fetchAccount = createAsyncThunk(
+  "/user/account",
+  async (thunkAPI) => {
+    try {
+      const response = await axios.post(FETCH_ACCOUNT);
+      return response.data;
+    } catch (error) {
+      thunkAPI.rejectWithValue(error.response);
+    }
+  }
+);
 
 export const userSlice = createSlice({
   name: "userSlice",
-  initialState: {},
+  initialState: {
+    accountStatus: "idle",
+    account: null,
+  },
   reducers: {},
-  extraReducers: {},
+  extraReducers: {
+    [fetchAccount.pending]: (state) => {
+      state.accountStatus = "loading";
+    },
+    [fetchAccount.fulfilled]: (state, action) => {
+      state.accountStatus = "success";
+      state.account = action.payload.account;
+    },
+    [fetchAccount.rejected]: (state, action) => {
+      state.accountStatus = "error";
+      state.error = action.payload;
+    },
+  },
 });
 
 export default userSlice.reducer;
