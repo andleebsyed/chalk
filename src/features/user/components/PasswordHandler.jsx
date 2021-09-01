de43a8ac74f4095a391f497cfd3cb171d2bd373a
Syed Andleeb <andleebsyed75@gmail.com>
2021-09-01 18:43:06 +0530

    update: password valiation function added to check for required parts in a password

--- src/features/user/components/PasswordHandler.jsx
+++ src/features/user/components/PasswordHandler.jsx
@@ -4,13 +4,48 @@ import { updatePassword } from '../userSlice'
 
 export function PasswordHandler() {
     const { updatePasswordStatus, error } = useSelector(state => state.user)
+    const [passwordsError, setPasswordsError] = useState({
+        status: "hidden"
+    })
     const [passwords, setPasswords] = useState({})
-    console.log({ error })
     const dispatch = useDispatch()
+    function PasswordHandler() {
+        if (passwords.newPassword.length < 6) {
+            setPasswordsError({
+                ...passwordsError,
+                message: "Password must be atleast of 6 characters",
+                status: "block",
+            });
+        } else if (passwords.newPassword !== passwords.confirmNewPassword) {
+            setPasswordsError({
+                ...passwordsError,
+                message: "Passwords didn't match",
+                status: "block",
+            });
+        } else if (passwords.newPassword.match("[0-9]+") === null) {
+            setPasswordsError({
+                ...passwordsError,
+                message: "Password must contain a number",
+                status: "block",
+            });
+        } else if (passwords.newPassword.match("(?=.*[A-Z])") === null) {
+            setPasswordsError({
+                ...passwordsError,
+                message: "Password must contain a Capital letter",
+                status: "block",
+            });
+        } else {
+            setPasswordsError({ ...passwordsError, status: "hidden" });
+            return true;
+        }
+    }
     function PasswordResetHandler(event) {
         event.preventDefault()
-        console.log({ passwords })
-        dispatch(updatePassword({ oldPassword: passwords.currentPassword, newPassword: passwords.newPassword }))
+        const passwordAllowed = PasswordHandler()
+        if (passwordAllowed) {
+            dispatch(updatePassword({ oldPassword: passwords.currentPassword, newPassword: passwords.newPassword }))
+
+        }
 
     }
     return (
@@ -19,10 +54,7 @@ export function PasswordHandler() {
         >
             <div className="border-4 p-2 theme-color-border flex flex-col justify-between m-4 sm:m-8 breakpoint-acc:m-4  breakpoint-acc:w-[40vw] rounded  breakpoint-acc:my-8">
                 <p className="font-bold text-lg">Reset Password</p>
-
-                {/* <p className={error ? "text-red-600 font-bold" : "text-blue font-bold"} >
-                    {updatePasswordStatus}
-                </p> */}
+                <p className={`${passwordsError.status} text-red-600 font-bold`}>{passwordsError.message}</p>
                 {error ?
                     <p className="text-red-600 font-bold">{error}</p> :
                     <p className="text-blue font-bold">{updatePasswordStatus}</p>
