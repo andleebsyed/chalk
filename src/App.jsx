552a0df0092b8ec95433f5a8f3797ad4ca8f9114
Syed Andleeb <andleebsyed75@gmail.com>
2021-09-01 09:38:26 +0530

    update: Moved header to homepage and will render only when user is authorized

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Routes, Route, useNavigate } from "react-router-dom";
-import { ThemeToggle } from "./common/components/Header";
+import { Header, ThemeToggle } from "./common/components/Header";
 import { Homepage } from "./common/components/Homepage";
 import { Landing } from "./common/components/Landing";
 import { setAuthSetup } from "./features/auth/authSlice";
 import { Login } from "./features/auth/login/Login";
 import { Signup } from "./features/auth/signup/Signup";
+import { Account } from "./features/user/Account";
 import { setupAuthExceptionHandler, setUpAuthHeaderForServiceCalls } from "./services/users";
 const App = () => {
   const { authorized } = useSelector(state => state.auth)
@@ -21,17 +22,20 @@ const App = () => {
   }, [dispatch, navigate]);
   return (
     <main className="text-black dark:text-white">
-      {!authorized && (
+      {!authorized ? (
         <div className="p-2 fixed  flex   min-w-screen right-0 h-9">
           <ThemeToggle />
-        </div>
-      )}
+        </div>)
+        : (
+          <Header />
+        )}
 
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/home/*" element={<Homepage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/account" element={<Account />} />
       </Routes>
     </main>
   );
--- src/common/components/Homepage.jsx
+++ src/common/components/Homepage.jsx
@@ -1,23 +1,21 @@
 import React from "react";
 import { Route, Routes } from "react-router";
 import { Notes } from "../../features/notes/AllNotes";
-import { Header } from "./Header";
 export function Homepage() {
-    function HomeContent() {
-        return (
-          <div className="text-center flex flex-col items-center min-h-screen justify-center container mx-auto">
-            <h1>Welcome to chalk</h1>
-          </div>
-        );
-    }
+  function HomeContent() {
+    return (
+      <div className="text-center flex flex-col items-center min-h-screen justify-center container mx-auto">
+        <h1>Welcome to chalk</h1>
+      </div>
+    );
+  }
   return (
     <div>
-      <Header />
-          <Routes>
-              <Route path="/" element={<HomeContent />} />
+      <Routes>
+        <Route path="/" element={<HomeContent />} />
         <Route path="/notes" element={<Notes />} />
       </Routes>
-    
+
     </div>
   );
 }
