8c2db7f5e98bea54bc83349d1e427ae8d26d1c1a
Syed Andleeb <andleebsyed75@gmail.com>
2021-09-01 12:21:40 +0530

    style: added some styling to ProfileDetails Page

--- src/features/user/components/ProfileDetails.jsx
+++ src/features/user/components/ProfileDetails.jsx
@@ -13,14 +13,14 @@ export function ProfileData({ account }) {
     console.log({ account })
     // nameEl.current.value = 
     return (
-        <div>
-            <form
-            // onSubmit={AccountUpdateHandler}
-            >
-                <div className="profile-details width-adjust">
-                    <p className="label">Account</p>
+        // <div className="" >
+        <form
+        // onSubmit={AccountUpdateHandler}
+        >
+            <div className="border-4 p-2 border-selected-navitem-light dark:border-selected-navitem-dark flex flex-col justify-between w-[40vw] rounded m-4 my-8">
+                <p className="font-bold text-left text-lg">Account</p>
 
-                    {/* {action.isLoading && (
+                {/* {action.isLoading && (
                         <>
                             <div className="account-interaction-loader">
                                 <SetLoader />
@@ -30,55 +30,55 @@ export function ProfileData({ account }) {
                             </div>
                         </>
                     )} */}
-                    {/* <p className={updateMessage.styleClass}>{updateMessage.message}</p> */}
-                    <div className="holder">
-                        <label className="labels-acc" htmlFor="name">
-                            Name
-                        </label>
-                        <input
-                            ref={nameEl}
-                            // onChange={(e) => setNewUsername(e.target.value)}
-                            name="name"
-                            type="name"
-                            className="input-box acc-username"
-                            placeholder="name"
-                            required
-                        />
-                    </div>
-                    <div className="holder">
-                        <label className="labels-acc" htmlFor="username">
-                            Username
-                        </label>
-                        <input
-                            // ref={usernameEl}
-                            // onChange={(e) => setNewUsername(e.target.value)}
-                            name="username"
-                            type="username"
-                            className="input-box acc-username"
-                            placeholder="username"
-                            required
-                        />
-                    </div>
-                    <div className="holder">
-                        <label className="labels-acc" htmlFor="email">
-                            Email
-                        </label>
-                        <input
-                            // ref={emailEl}
-                            // onChange={(e) => setNewEmail(e.target.value)}
-                            name="email"
-                            type="email"
-                            className="input-box acc-password"
-                            placeholder="email"
-                            required
+                {/* <p className={updateMessage.styleClass}>{updateMessage.message}</p> */}
+                <section className="flex flex-col ">
+                    <label className="text-left font-bold" htmlFor="name">
+                        Name
+                    </label>
+                    <input
+                        ref={nameEl}
+                        // onChange={(e) => setNewUsername(e.target.value)}
+                        name="name"
+                        type="name"
+                        className="input-box"
+                        placeholder="name"
+                        required
+                    />
+                </section>
+                <section className="flex flex-col ">
+                    <label className="text-left font-bold" htmlFor="username">
+                        Username
+                    </label>
+                    <input
+                        // ref={usernameEl}
+                        // onChange={(e) => setNewUsername(e.target.value)}
+                        name="username"
+                        type="username"
+                        className="input-box"
+                        placeholder="username"
+                        required
+                    />
+                </section>
+                <section className="flex flex-col ">
+                    <label className="text-left font-bold" htmlFor="email">
+                        Email
+                    </label>
+                    <input
+                        // ref={emailEl}
+                        // onChange={(e) => setNewEmail(e.target.value)}
+                        name="email"
+                        type="email"
+                        className="input-box"
+                        placeholder="email"
+                        required
 
-                        />
-                    </div>
-                    <button type="submit" className="submit-button ">
-                        UPDATE
-                    </button>
-                </div>
-            </form>
-        </div>
+                    />
+                </section>
+                <button type="submit" className=" my-2 self-start cursor-pointer h-10 w-40  rounded font-bold bg-selected-navitem-light dark:bg-selected-navitem-dark  ">
+                    UPDATE
+                </button>
+            </div>
+        </form>
+        // </div>
     )
 }
