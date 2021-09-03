64d9a83225dd30fa7e3737e98677f79b31ffbf98
Syed Andleeb <andleebsyed75@gmail.com>
2021-09-03 17:58:55 +0530

    update: added color palatte to the note making component

--- src/features/notes/createNote.jsx
+++ src/features/notes/createNote.jsx
@@ -4,10 +4,24 @@ import { IoColorPaletteSharp } from 'react-icons/io5'
 import { MdLabel } from 'react-icons/md'
 import { BiImageAlt } from 'react-icons/bi'
 import { RiPushpin2Fill } from 'react-icons/ri'
+export function ColorPallate() {
+    const noteColors = ["palette-yellow", "palette-blue", "palette-red", "palette-purple"]
+    return (
+        <div className="flex absolute">
+            {noteColors.map(noteColor =>
+                <div key={noteColor} className={`bg-${noteColor} rounded-full w-8 h-8 m-1`}>
+                </div>
+            )}
+        </div>
+
+
+    )
+}
 export function CreateNote() {
     const [pinned, setPinned] = useState(false)
+    const [palatte, setPalatte] = useState(false)
     return (
-        <div className=" m-8  p-2 w-[90%]  max-w-[600px] h-[152px] rounded-lg box-shadow-light dark:box-shadow-dark bg-opacity-20">
+        <div className="m-8 p-2 w-[90%]  max-w-[600px] h-[152px] rounded-lg box-shadow-light dark:box-shadow-dark bg-opacity-20">
 
             <form className="flex flex-col p-2 outline-none " onSubmit={(e) => e.preventDefault()}>
                 <section className="flex mb-1">
@@ -24,9 +38,16 @@ export function CreateNote() {
                 </section>
                 <input type="text" placeholder="Take a note..." className="h-[57px] p-2 outline-none dark:bg-dark-1 dark:text-white" />
                 <section className="p-2 flex w-[50%] justify-between mt-1">
-                    <button title="Change color">
+                    <div title="Change color" className="" >
                         <IoColorPaletteSharp size={22} />
-                    </button>
+                        <div className=" relative">
+                            <ColorPallate />
+                        </div>
+
+                    </div>
+                    {/* <div className={palatte ? "block relative top-6 right-10" : "hidden"}> */}
+
+                    {/* </div> */}
                     <button title="Add Label">
                         <MdLabel size={22} />
                     </button>
