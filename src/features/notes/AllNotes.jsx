fbc21cd6c65a80902d065908476e4c933dbcb6bc
Syed Andleeb <andleebsyed75@gmail.com>
2021-09-05 10:12:26 +0530

    update: every note of a certain user is being iterated and passed as prop to what we have as a single note component

--- src/features/notes/AllNotes.jsx
+++ src/features/notes/AllNotes.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { ShowNote } from "./ShowNote";
 export function Notes() {
+  const { notes, } = useSelector(state => state.notes)
   return (
     <div>
-      <h1>i am inside all notes</h1>
+      {notes?.map(note =>
+        <div key={note?._id}>
+          <ShowNote note={note} />
+        </div>
+      )}
     </div>
   );
 }
