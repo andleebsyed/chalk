7416d4c11fc95d19e38b71c2885d837dec543db3
Syed Andleeb <andleebsyed75@gmail.com>
2021-09-05 16:49:06 +0530

    update: when note is saved successfully, made the note Creation componet reset it's data

--- src/features/notes/createNote.jsx
+++ src/features/notes/createNote.jsx
@@ -1,11 +1,11 @@
-import React, { useRef, useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import { RiPushpin2Line } from 'react-icons/ri'
 import { IoColorPaletteSharp } from 'react-icons/io5'
 import { BiImageAlt } from 'react-icons/bi'
 import { RiPushpin2Fill } from 'react-icons/ri'
 import { LabelModal } from "../../common/components/LabelModal/LabelModal"
 import { useDispatch, useSelector } from "react-redux"
-import { addNote, removeFromChosenLabels } from "./notesSlice"
+import { addNote, removeFromChosenLabels, resetChosenLabels } from "./notesSlice"
 import { GiCancel } from "react-icons/gi"
 // export function ColorPallate() {
 //     const noteColors = ["white", "palette-yellow", "palette-blue", "palette-red", "palette-purple"]
@@ -16,7 +16,7 @@ import { GiCancel } from "react-icons/gi"
 //     )
 // }
 export function CreateNote() {
-    const { error } = useSelector(state => state.notes)
+    const { error, status } = useSelector(state => state.notes)
     const [pinned, setPinned] = useState(false)
     const { chosenLabels } = useSelector(state => state.notes)
     const [noteData, setNoteData] = useState({
@@ -34,6 +34,7 @@ export function CreateNote() {
     });
     const dispatch = useDispatch()
     const imageRef = useRef(null)
+    const formRef = useRef(null)
     // const [palatte, setPalatte] = useState(false)
     // const [noteColor, setNoteColor] = useState("white")
     // const noteColors = ["palette-blue", "palette-yellow", "palette-red", "palette-purple"]
@@ -41,21 +42,37 @@ export function CreateNote() {
         console.log({ error })
     }
     console.log({ pinned })
-    function submitNote(e) {
+    async function submitNote(e) {
         e.preventDefault()
         console.log("out choosen labels ", chosenLabels)
-        setNoteData({ ...noteData, color: "white", image })
+        setNoteData({ ...noteData, color: "white" })
         console.log({ noteData }, "note data")
         let formData = new FormData();
         formData.append("title", noteData.title)
         formData.append("content", noteData.content)
-        formData.append("image", noteData?.image)
+        formData.append("image", image)
         formData.append("pinned", pinned)
         formData.append("labels", JSON.stringify(chosenLabels))
         formData.append("color", noteData?.color)
-        dispatch(addNote({ formData }))
+        await dispatch(addNote({ formData }))
+        // if (status === "success") {
+        //     formRef.current.reset()
+        //     setImageData({ url: null, showStatus: "hidden" })
+        //     setImage(null)
+        //     setPinned(false)
+        //     dispatch(resetChosenLabels())
+        // }
 
     }
+    useEffect(() => {
+        if (status === "success") {
+            formRef.current.reset()
+            setImageData({ url: null, showStatus: "hidden" })
+            setImage(null)
+            setPinned(false)
+            dispatch(resetChosenLabels())
+        }
+    }, [status])
     function fileUploadHandler(e) {
         if (e.target.files && e.target.files[0]) {
             setImage(e.target.files[0])
@@ -73,8 +90,10 @@ export function CreateNote() {
     return (
         // dark:bg-${noteColor === ("white" || "off-white") ? "dark-1" : noteColor}
         <div className={`self-center m-8 p-2 w-[90%] bg-white dark:bg-dark-1  max-w-[600px] min-h-[152px] rounded-lg  box-shadow-light dark:box-shadow-dark `}>
-
-            <form className="flex flex-col p-2 outline-none  " onSubmit={(e) => submitNote(e)}>
+            {error && <p className="text-red-600 font-bold pl-3">Could not save the note!! Your image size might be too big. Try changing the image or simply try again.</p>}
+            {status === "success" && <p className="text-blue font-bold">Note saved successfully</p>}
+            {status === "pending" && <p className="text-palette-yellow font-bold">Please wait while we save your note...</p>}
+            <form ref={formRef} className="flex flex-col p-2 outline-none  " onSubmit={(e) => submitNote(e)}>
                 <section className="flex mb-1">
                     <input type="text" placeholder="Title" className={`h-[36px] w-full p-2 outline-none bg-white dark:bg-dark-1`} onChange={(e) => setNoteData({ ...noteData, title: e.target.value })} required />
                     <button className="ml-auto" onClick={(e) => { e.preventDefault(); setPinned(!pinned) }}>
@@ -86,13 +105,7 @@ export function CreateNote() {
                     </button>
                 </section>
                 <input type="text" placeholder="Take a note..." className={`h-[57px] p-2 outline-none  bg-white dark:bg-dark-1 dark:text-white `} onChange={(e) => setNoteData({ ...noteData, content: e.target.value })} required />
-                <div className="flex flex-wrap">
-                    {chosenLabels?.map(chosenLabel => <div key={chosenLabel?._id} className="text-sm p-1 pt-1 flex rounded-lg m-1 bg-selected-navitem-light dark:bg-selected-navitem-dark">
-                        <span className="self-center">{chosenLabel.labelName}</span>
-                        <button className="text-xs p-2 self-center" onClick={(e) => { e.preventDefault(); dispatch(removeFromChosenLabels({ removedLabel: chosenLabel })) }}>X</button>
-                    </div>)
-                    }
-                </div>
+
 
                 <div className={`${imageData.showStatus} `}>
                     <button className="relative top-11 left-1 bg-red-600 bg-opacity-40 rounded p-2 " onClick={(e) => { e.preventDefault(); setImageData({ url: null, showStatus: "hidden" }); setImage(null) }}> <GiCancel size={26} /></button>
@@ -103,6 +116,13 @@ export function CreateNote() {
                         src={imageData.url}
                         className={`${imageData.showStatus}  self-center mb-4`}
                     />
+                </div>
+                <div className="flex flex-wrap">
+                    {chosenLabels?.map(chosenLabel => <div key={chosenLabel?._id} className="text-sm p-1 pt-1 flex rounded-lg m-1 bg-selected-navitem-light dark:bg-selected-navitem-dark">
+                        <span className="self-center">{chosenLabel.labelName}</span>
+                        <button className="text-xs p-2 self-center" onClick={(e) => { e.preventDefault(); dispatch(removeFromChosenLabels({ removedLabel: chosenLabel })) }}>X</button>
+                    </div>)
+                    }
                 </div>
 
                 <section className="p-2 flex w-[50%] justify-between mt-1">
