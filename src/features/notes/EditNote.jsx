c3275209f15aa5a9f36fa891863f9cbd8888b300
Syed Andleeb <andleebsyed75@gmail.com>
2021-09-06 12:17:20 +0530

    update: added elements to the edit note Modal

--- src/features/notes/EditNote.jsx
+++ src/features/notes/EditNote.jsx
@@ -1,18 +1,102 @@
 /* eslint-disable react/prop-types */
-import React from "react"
+import React, { useEffect, useRef, useState } from "react"
+import { BiImageAlt } from "react-icons/bi";
+import { IoColorPaletteSharp } from "react-icons/io5";
+import { RiPushpin2Fill, RiPushpin2Line } from "react-icons/ri";
 import { useDispatch, useSelector } from "react-redux";
-import { disableEditModal } from "./notesSlice";
+import { LabelModal } from "../../common/components/LabelModal/LabelModal";
+import { disableEditModal, removeFromChosenLabels } from "./notesSlice";
 export function EditNote() {
 
     const { noteToEdit, editNoteModalStatus } = useSelector(state => state.notes)
     console.log(noteToEdit, editNoteModalStatus)
     const dispatch = useDispatch()
+    const formRef = useRef(null)
+    const [pinned, setPinned] = useState(false)
+    const [noteData, setNoteData] = useState(null)
+
+
+    useEffect(() => {
+        if (noteToEdit) {
+            setNoteData({
+                ...noteData, title: noteToEdit.title,
+                content: noteToEdit.content,
+                image: noteToEdit.image,
+                pinned: noteToEdit.pinned,
+                labels: noteToEdit.labels,
+                color: null
+            })
+
+        }
+    }, [noteToEdit])
+    function submitNote(e) {
+        e.preventDefault()
+    }
+    function fileUploadHandler() {
+
+    }
     return (
-        editNoteModalStatus && <div onClick={() => { dispatch(disableEditModal()) }} className=" fixed top-0 left-0 bottom-0 right-0  bg-dark-1 bg-opacity-50 flex items-center justify-center min-h-screen min-w-screen ">
-            <div className="border border-red-500 " onClick={(e) => e.stopPropagation()}>
+        editNoteModalStatus && noteData && <div onClick={() => { dispatch(disableEditModal()) }} className="  fixed top-0 left-0 bottom-0 right-0  bg-dark-1 bg-opacity-50 flex items-center justify-center min-h-screen min-w-screen ">
+            <div className={`self-center m-8 p-2 w-[90%] bg-white dark:bg-dark-1  max-w-[600px] min-h-[152px] rounded-lg  box-shadow-light dark:box-shadow-dark `} onClick={(e) => e.stopPropagation()}>
+                {/* {error && <p className="text-red-600 font-bold pl-3">Could not save the note!! Your image size might be too big. Try changing the image or simply try again.</p>}
+            {status === "success" && <p className="text-blue font-bold">Note saved successfully</p>}
+            {status === "pending" && <p className="text-palette-yellow font-bold">Please wait while we save your note...</p>} */}
                 <button onClick={(e) => { e.stopPropagation(); dispatch(disableEditModal()) }}>X</button>
-                <p>this is where we willl edit a note</p>
-                <p>{noteToEdit?.content}</p>
+                <form ref={formRef} className="flex flex-col p-2 outline-none   " onSubmit={(e) => { e.stopPropagation(); submitNote(e) }}>
+                    <section className="flex mb-1">
+                        <input type="text" placeholder="Title" className={`h-[36px] w-full p-2 outline-none bg-white dark:bg-dark-1`} value={noteData.title} onChange={(e) => setNoteData({ ...noteData, title: e.target.value })} required />
+                        <button className="ml-auto" onClick={(e) => { e.preventDefault(); setPinned(!pinned) }}>
+                            {noteToEdit.pinned ?
+                                <RiPushpin2Fill size={28} />
+                                :
+                                <RiPushpin2Line size={28} />
+                            }
+                        </button>
+                    </section>
+                    <input type="text" placeholder="Take a note..." className={`h-[57px] p-2 outline-none  bg-white dark:bg-dark-1 dark:text-white `} value={noteData.content} onChange={(e) => setNoteData({ ...noteData, content: e.target.value })} required />
+
+
+                    {noteData.image && <div className="overflow-y-scroll max-h-[30rem]" ><img src={noteData.image} /></div>}
+                    <div className="flex flex-wrap">
+                        {noteData.labels?.map(chosenLabel => <div key={chosenLabel?._id} className="text-sm p-1 pt-1 flex rounded-lg m-1 bg-selected-navitem-light dark:bg-selected-navitem-dark">
+                            <span className="self-center">{chosenLabel.labelName}</span>
+                            <button className="text-xs p-2 self-center" onClick={(e) => { e.preventDefault(); dispatch(removeFromChosenLabels({ removedLabel: chosenLabel })) }}>X</button>
+                        </div>)
+                        }
+                    </div>
+
+                    <section className="p-2 flex w-[50%] justify-between mt-1">
+                        <div title="Change color" className="group" >
+                            <IoColorPaletteSharp size={22} />
+                            {/* <div className=" relative hidden group-hover:block">
+                          
+                            <div className="flex absolute border-2 rounded border-selected-navitem-light dark:border-selected-navitem-dark">
+                                <div onClick={() => setNoteColor(() => localStorage.getItem("theme") === "dark" ? "black" : "white")} className={`${localStorage.getItem("theme") === "dark" ? "text-black" : "text-white"} border border-black dark:border-white rounded-full w-8 h-8 m-1`}>
+                                </div>
+                                {noteColors.map(singleColor =>
+                                
+                                    <div onClick={() => setNoteColor(singleColor)} key={singleColor} className={`bg-${singleColor} border border-black dark:border-white  rounded-full w-8 h-8 m-1`}>
+                                    </div>
+                        
+                                )}
+                            </div>
+                        </div> */}
+
+                        </div>
+                        <LabelModal />
+                        <label className="cursor-pointer" title="Choose Image">
+                            <BiImageAlt size={22} />
+                            <input type="file" id="img"
+                                name="img"
+                                accept="image/*"
+                                className="hidden" onChange={fileUploadHandler} />
+                        </label>
+
+
+
+                    </section>
+                    <input type="submit" value="Edit Note" className="ml-auto bg-selected-navitem-light dark:bg-selected-navitem-dark rounded p-2 cursor-pointer" />
+                </form>
             </div>
         </div>
 
