9cd5d6f8eee9f05862e5f3c59fb64a126bb28db0
Syed Andleeb <andleebsyed75@gmail.com>
2021-09-07 12:41:37 +0530

    refactor: removed unused code

--- src/features/notes/PinnedNotes.jsx
+++ src/features/notes/PinnedNotes.jsx
@@ -4,16 +4,19 @@ import { ShowNote } from "./ShowNote"
 export function PinnedNotes() {
     const { allNotes } = useSelector(state => state.notes)
     const pinnedNotes = allNotes.filter(note => note.pinned === true)
+
     return (
-        pinnedNotes.length !== 0 &&
+        pinnedNotes?.length !== 0 &&
         <>
             <h1 className="text-blue font-bold self-center">Pinned Notes</h1>
             <div className="flex flex-wrap justify-center sm:justify-start">
-                {pinnedNotes?.map(note =>
-                    <div key={note?._id}>
-                        <ShowNote note={note} />
-                    </div>
-                )}
+                {
+                    pinnedNotes?.map(note =>
+                        <div key={note?._id}>
+                            <ShowNote note={note} />
+                        </div>
+                    )
+                }
             </div>
         </>
     )
