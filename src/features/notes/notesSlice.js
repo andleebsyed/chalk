ec82953642e34ee393a497e871aeca974a26b865
Syed Andleeb <andleebsyed75@gmail.com>
2021-09-09 21:10:17 +0530

    update: changed iniitial value of allNotes property as null, which will let us to create an optimal conditional for showing loader on view

--- src/features/notes/notesSlice.js
+++ src/features/notes/notesSlice.js
@@ -125,7 +125,7 @@ export const notesSlice = createSlice({
   name: "notesSlice",
   initialState: {
     notesFetchstatus: "idle",
-    allNotes: [],
+    allNotes: null,
     allNotesBackup: [],
     notes: [],
     pinnedNotes: [],
